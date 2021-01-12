77fd53d064723c934d77e6cbf5c55f17fec223fd
Anand Rajpara <anand.rajpara@outlook.com>
2021-01-12 22:41:16 +0530

    Add project files.

---
+++ StackConsoleApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generik Stack Class Operations");
+
+            // Stack Generic Call in C#
+            Stack<int> testStack = new Stack<int>();
+            int count = 0;
+
+            Console.WriteLine("Enter how many element for stack");
+            var inputCount = Console.ReadLine();
+
+            if(Int32.TryParse(inputCount, out count))
+            {
+                for(int i = 0; i < count; i++)
+                {
+                    testStack.Push(Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Total Element in Stack is {testStack.Count}");
+            Console.WriteLine($"Top Element on Stack is {testStack.Peek()} - Peek Operation");            
+            
+            while(testStack.Count > 0)
+            {
+                Console.WriteLine($"Element in Stack is : {testStack.Pop()} - Pop Operation");
+            }
+
+            // Stack with linked list
+            StackLinkedList stackLinkedList = new StackLinkedList();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter how many element for stack");
+            var inputStackCount = Console.ReadLine();
+
+            if (Int32.TryParse(inputStackCount, out count))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    stackLinkedList.PUSH(Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+            Console.WriteLine($"Top Element on Stack is {stackLinkedList.PeeK()} - Peek Operation");
+
+            while (stackLinkedList.top != null)
+            {
+                Console.WriteLine($"Element in Stack is : {stackLinkedList.POP()} - Pop Operation");
+            }
+
+
+        }
+    }
+}
---
+++ StackConsoleApplication/StackLinkedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackConsoleApplication
+{
+    public class Node
+    {
+        public int data;
+        public Node link;
+    }
+
+    public class StackLinkedList
+    {
+        public Node top;
+
+        public StackLinkedList()
+        {
+            this.top = null;
+        }
+
+        public void PUSH(int x)
+        {
+            Node node = new Node();
+
+            node.data = x;
+            node.link = top;
+
+            top = node;
+        }
+
+        public int PeeK()
+        {
+            if(top != null)
+            {
+                return top.data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+        }
+
+        public int POP()
+        {
+            if(top != null)
+            {
+                int data = top.data;
+                top = top.link;
+                return data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+        }
+    }
+}
